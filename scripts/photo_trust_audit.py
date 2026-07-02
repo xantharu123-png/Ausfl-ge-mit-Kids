aeacb497028cd3b0fd7d709b2d325b185b2ffdce
Miroslav Mikulic <miroslav.mikulic@gmail.com>
2026-07-02 16:35:40 +0200

    Add verified Schleissheim and Piediluco photos

--- scripts/photo_trust_audit.py
+++ scripts/photo_trust_audit.py
@@ -169,7 +169,7 @@ KNOWN_GENERIC_RE = re.compile(
     r"tren_a_las_nubes_cruzando_viaducto_la_polvorilla|"
     r"woerther_see_in_bayern|berlin_neue_synagoge_2005|"
     r"st_michaels_church_hildesheim|prt\.png|"
-    r"venice_lagoon_december)",
+    r"venice_lagoon_december|valvasor_leonstain_1680)",
     re.I,
 )
 
